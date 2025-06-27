d7f490b8210f667711f3c2089ff707d0f9d9e6e8
David Gonzalo Mercado Juanes <dmercado@abc.gob.bo>
2025-06-26 23:05:58 -0400

    [CLASE 10] Validación de Datos y Manejo de Errores HTTP en Symfony

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -11,14 +11,23 @@ use App\Repository\PostRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
 #[ORM\Entity(repositoryClass: PostRepository::class)]
 #[ApiResource(
     operations: [
-        new Get(),
-        new GetCollection(),
+        new Get(
+            normalizationContext: ['groups' => ['read:item']], // GET
+        ),
+        new GetCollection(
+            normalizationContext: ['groups' => ['read:collection']], // GET
+        ),
         new Store(),
         new Patch(),
-    ]
+    ],
+    // normalizationContext: ['groups' => ['read']], // GET
+    denormalizationContext: ['groups' => ['write']], // POST, PATCH, PUT
 )]
 
 class Post
@@ -26,16 +35,23 @@ class Post
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['read', 'write'])]
+    #[Assert\NotBlank]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['read:item', 'write'])]
+    #[Assert\NotBlank]
     private ?string $body = null;
 
     #[ORM\ManyToOne(inversedBy: 'posts')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['read', 'write'])]
+    #[Assert\NotBlank]
     private ?Category $category = null;
 
     public function getId(): ?int
@@ -58,6 +74,15 @@ class Post
     public function getBody(): ?string
     {
         return $this->body;
+    }
+    
+    #[Groups(['read:collection'])]
+    public function getSummary($len = 70): ?string
+    {
+        if (mb_strlen($this->body) <= $len) {
+            return $this->body;
+        }
+        return mb_substr($this->body, 0, 70) . '...';
     }
 
     public function setBody(string $body): static
